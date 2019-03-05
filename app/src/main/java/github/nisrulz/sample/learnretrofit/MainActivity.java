0c6d2a4ea7bfc3df0e4f4d734209f3814fece21d
Nishant Srivastava <nisrulz@gmail.com>
2019-03-05 23:28:40 +0100

    Updated project
    - Migrated to AndroidX

--- app/src/main/java/github/nisrulz/sample/learnretrofit/MainActivity.java
+++ app/src/main/java/github/nisrulz/sample/learnretrofit/MainActivity.java
@@ -1,10 +1,10 @@
 package github.nisrulz.sample.learnretrofit;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 import github.nisrulz.sample.learnretrofit.data.ApiUtils;
